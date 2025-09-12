3530d90a02714c02334d53f771d44e880d680e3c
surufel <henriquefrancasmm@gmail.com>
2025-09-12 12:26:46 -0300

    [+] Adicionado instâncias de ListaDeProcessos, listas de cada nível de prioridade, e uma de processos bloqueadas, e um contador de ciclos de alta prioridade (int)

--- src/src/Scheduler.java
+++ src/src/Scheduler.java
@@ -1,19 +1,14 @@
 public class Scheduler {
 
+    ListaDeProcessos lista_alta_prioridade = new ListaDeProcessos();
+    ListaDeProcessos lista_media_prioridade = new ListaDeProcessos();
+    ListaDeProcessos lista_baixa_prioridade = new ListaDeProcessos();
+    ListaDeProcessos lista_bloqueados = new ListaDeProcessos();
+
+    int contador_ciclos_alta_prioridade;
+
     public static void executarCicloDeCPU(){
 
     }
 
 }
-/*
-                Atributos do Scheduler:
-
-    - Três instâncias da sua ListaDeProcessos: uma para cada nível
-    de prioridade (lista_alta_prioridade, lista_media_prioridade,
-    lista_baixa_prioridade).
-
-    - Uma ListaDeProcessos para processos bloqueados
-    (lista_bloqueados).
-
-    - Um contador_ciclos_alta_prioridade (int)
-*/
