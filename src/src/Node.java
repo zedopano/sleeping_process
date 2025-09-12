0c844d9e12a95b405d61ad231ae737889209cb2b
surufel <henriquefrancasmm@gmail.com>
2025-09-12 11:45:47 -0300

    [+] Classe Node para fazer uso das Est. de Dados

---
+++ src/src/Node.java
@@ -0,0 +1,12 @@
+public class Node {
+    int data;
+    Node proximo;
+    Node anterior;
+
+    public Node(int dado){
+        this.proximo = null;
+        this.anterior = null;
+        this.data = data;
+
+    }
+}
