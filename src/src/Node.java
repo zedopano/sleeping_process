0f25c86e58fdb8ea9d2956d69266f3aa0bc82e9d
zaast1 <darkq1408@gmail.com>
2025-09-16 13:11:13 -0300

    lista circular e Node

--- src/src/Node.java
+++ src/src/Node.java
@@ -1,12 +1,36 @@
 public class Node {
-    int valor;
-    Node proximo;
-    Node anterior;
 
-    public Node(int valor){
-        this.valor = valor;
+    private Object dado;
+    private Node proximo;
+    private Node anterior;
+
+    public Node(Object dado) {
+        this.dado = dado;
         this.proximo = null;
         this.anterior = null;
+    }
 
+    public Object getDado() {
+        return this.dado;
+    }
+
+    public void setDado(Object dado) {
+        this.dado = dado;
+    }
+
+    public Node getProximo() {
+        return this.proximo;
+    }
+
+    public void setProximo(Node proximo) {
+        this.proximo = proximo;
+    }
+
+    public Node getAnterior() {
+        return this.anterior;
+    }
+
+    public void setAnterior(Node anterior) {
+        this.anterior = anterior;
     }
 }
--- src/src/listaCircular.java
+++ src/src/listaCircular.java
@@ -1,21 +1,63 @@
 public class listaCircular {
-    Node inicio;
+
+    private Node cauda;
+    private int tamanho;
 
     public listaCircular() {
-        this.inicio = null;
+        this.cauda = null;
+        this.tamanho = 0;
     }
 
-    public void inserir(int dado) {
-        Node novoNo = new Node(dado);
-        if (inicio == null) {
-            inicio = novoNo;
-        } else {
-            Node ultimo = inicio.anterior;
-            ultimo.proximo = novoNo;
-            novoNo.anterior = ultimo;
-            novoNo.proximo = inicio;
-            inicio.anterior = novoNo;
+    public boolean isVazia() {
+        return this.tamanho == 0;
+    }
 
+    public int getTamanho() {
+        return this.tamanho;
+    }
+
+    public void inserirNoInicio(Object dado) {
+        Node novoNo = new Node(dado);
+        if (isVazia()) {
+            this.cauda = novoNo;
+            novoNo.setProximo(novoNo);
+            novoNo.setAnterior(novoNo);
+        } else {
+            Node cabeca = cauda.getProximo();
+
+            novoNo.setProximo(cabeca);
+            cabeca.setAnterior(novoNo);
+            novoNo.setAnterior(cauda);
+            cauda.setProximo(novoNo);
         }
+        this.tamanho++;
+    }
+
+    public void inserirNoFim(Object dado) {
+        inserirNoInicio(dado);
+
+        if (tamanho > 1) {
+            this.cauda = this.cauda.getProximo();
+        }
+    }
+
+    public void exibirLista() {
+        if (isVazia()) {
+            System.out.println("A lista está vazia.");
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder("[");
+        Node atual = cauda.getProximo();
+
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(atual.getDado());
+            if (i < tamanho - 1) {
+                sb.append(" <-> ");
+            }
+            atual = atual.getProximo();
+        }
+        sb.append("]");
+        System.out.println(sb.toString());
     }
 }
